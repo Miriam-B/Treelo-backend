47b03f41385ccb384d481bf95afd99f8fc564ac4
Miriam Baranovska <maryam1649@gmail.com>
2021-06-14 13:06:21 +0300

    first personal commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+// Database Name
+module.exports = {
+    'dbURL': 'mongodb+srv://treelo:oleert@sprint4ca.qnzxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'dbURL': 'mongodb+srv://treelo:oleert@sprint4ca.qnzxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+// Database Name
+}
